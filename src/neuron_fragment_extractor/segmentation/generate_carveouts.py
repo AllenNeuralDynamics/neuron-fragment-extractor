66b4427968015e578e554b2bdd697a06297a6021
anna-grim <anna.grim@alleninstitute.org>
2026-02-21 05:18:23 +0000

    feat: initialize cloud ome-zarr

--- src/neuron_fragment_extractor/segmentation/generate_carveouts.py
+++ src/neuron_fragment_extractor/segmentation/generate_carveouts.py
@@ -22,29 +22,25 @@ import tifffile
 import threading
 
 from neuron_fragment_extractor.graph_classes import SkeletonGraph
-from neuron_fragment_extractor.utils.img_util import TensorStoreReader
+from neuron_fragment_extractor.utils.img_util import TensorStoreImage
 from neuron_fragment_extractor.utils import img_util
 
 
 def main():
-    # Paths
-    carveout_raw_path = os.path.join(output_dir, "input.zarr")
-    carveout_mask_path = os.path.join(output_dir, "mask.zarr")
-
     # Initializations
     gt_graph = load_skeletons()
-    src_img = TensorStoreReader(img_path)
-    dst_img = np.zeros(src_img.shape())
-    dst_mask = np.zeros(src_img.shape())
-    print("\ncarveout_img.shape:", dst_img.shape)
+    src_img = TensorStoreImage(img_path)
+    dst_img = init_carveout("input.zarr", src_img.shape())
+    dst_mask = init_carveout("mask.zarr", src_img.shape())
+    stop
 
     # Generate carveouts
     carve_out_pipeline = CarveOutPipeline(gt_graph, radial_shape)
     carve_out_pipeline.generate_raw(src_img, dst_img)
     carve_out_pipeline.generate_mask(dst_mask)
 
-    tifffile.imwrite("/home/jupyter/raw.tiff", dst_img[0, 0].astype(np.uint16))
-    tifffile.imwrite("/home/jupyter/mask.tiff", dst_mask[0, 0].astype(np.uint8))
+    tifffile.imwrite("/home/jupyter/raw.tiff", dst_img[0, 0, 0:512].astype(np.uint16))
+    tifffile.imwrite("/home/jupyter/mask.tiff", dst_mask[0, 0, 0:512].astype(np.uint8))
 
     # Write metadata
 
@@ -55,11 +51,30 @@ class CarveOutPipeline:
         self,
         graph,
         radial_shape,
-        num_readers=1,
+        num_readers=16,
         num_writers=1,
-        prefetch=64,
-        step_size=10
+        prefetch=128,
+        step_size=20
     ):
+        """
+        Instantiates a CarveOutPipeline object.
+
+        Parameters
+        ----------
+        graph : SkeletonGraph
+            Graph to be traversed to generate carve-out regions.
+        radial_shape : Tuple[int]
+            Shape of region centered about skeleton to be carved out.
+        num_reader : int, optional
+            Number of threads used to read image patches. Default is 16.
+        num_writers : int, optional
+            Number of threads used to write image patches. Default is 1.
+        prefetch : int, optional
+            Number of image patches to be prefeteched. Default is 128.
+        step_size : float, optional
+            Distance (in microns) between carved-out regions, measured along
+            graph traversal. Default is 20.
+        """
         # Instance attributes
         self.graph = graph
         self.num_readers = num_readers
@@ -71,7 +86,7 @@ class CarveOutPipeline:
     def generate_raw(self, src_img, dst_img):
         def traverse():
             for node in self.traverse_graph():
-                if self.is_patch_contained(node, dst_img.shape[2:]):  # TEMP
+                if self.is_patch_contained(node, dst_img.shape[2:]):
                     slices_q.put(self.node_to_slices(node))
             for _ in range(self.num_readers):
                 slices_q.put(stop)
@@ -123,8 +138,11 @@ class CarveOutPipeline:
 
     def generate_mask(self, dst_img):
         def traverse():
+            """
+            Gets nodes to extract patches about by traversing the graph.
+            """
             for node in self.traverse_graph():
-                if self.is_patch_contained(node, dst_img.shape[2:]):  # TEMP
+                if self.is_patch_contained(node, dst_img.shape[2:]):
                     slices_q.put(self.node_to_slices(node))
             for _ in range(self.num_readers):
                 slices_q.put(stop)
@@ -201,7 +219,41 @@ class CarveOutPipeline:
 
 
 # --- Helpers ---
+def init_carveout(filename, img_shape):
+    """
+    Initializes empty image that carve-out is to be written to.
+
+    Parameters
+    ----------
+    filename : str
+        Name of OME-Zarr image to be created.
+    shape : Tuple[int]
+        Shape of image to be created.
+
+    Returns
+    -------
+    TensorStoreImage
+        Empty image that carve-out is to be written to.
+    """
+    img_path = f"{output_dir}/{brain_id}/blocks/block_000/{filename}"
+    img_util.init_omezarr_image(img_path, img_shape)
+    return TensorStoreImage(os.path.join(img_path, str(0)))
+
+
 def load_skeletons():
+    """
+    Loads the SWC files into a SkeletonGraph.
+
+    Parameters
+    ----------
+    swc_pointer : str
+        Path to SWC files to be loaded.
+
+    Returns
+    -------
+    graph : SkeletonGraph
+        Graph with specified SWC files loaded.
+    """
     gt_graph = SkeletonGraph()
     for swc_name in gt_swc_names:
         swc_path = os.path.join(gt_dir, swc_name)
@@ -212,14 +264,14 @@ def load_skeletons():
 if __name__ == "__main__":
     # Parameters
     brain_id = "802449"
-    gt_swc_names = ["00001.swc", "00002.swc"]  #["N002-802449-PP.swc"]
-    radial_shape = (30, 30, 30)   #(512, 512, 512)
+    gt_swc_names = ["00005.swc", "00013.swc"]  #["N002-802449-PP.swc"]
+    radial_shape = (32, 32, 32)   #(512, 512, 512)
     step_size = 10
 
     # Paths
     #gt_dir = f"gs://allen-nd-goog/ground_truth_tracings/{brain_id}/voxel"
     #img_path = "gs://allen-nd-goog/from_aind/exaSPIM_802449_2025-12-16_18-17-47_training-data/whole-brain/fused.zarr/0"
-    output_dir = f"gs://allen-nd-goog/from_aind/agrim-experimental/image_carveouts/{brain_id}"
+    output_dir = "gs://allen-nd-goog/from_aind/agrim-experimental/image-carveouts"
 
     gt_dir = "gs://allen-nd-goog/from_aind/training-data_2025-07-30/swcs/block_000/"
     img_path = "gs://allen-nd-goog/from_aind/training-data_2025-07-30/blocks/block_000/input.zarr/0"
